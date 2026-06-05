9c619c70f8092b9256940c4f75c01ab5903d6840
tomoda-dot <tomoda_a@yahoo.co.jp>
2026-06-05 11:35:54 +0900

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,3 @@
+// Supabase接続設定（Porte / ひとつぎ）
+const SUPABASE_URL = 'https://ukdbwxhvnmiibuwbrdel.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVrZGJ3eGh2bm1paWJ1d2JyZGVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA2MDUyNDgsImV4cCI6MjA5NjE4MTI0OH0.PEJdV8HDreqE28-i27j-TODNFJP0RGKFrrNRBT-rZag';
